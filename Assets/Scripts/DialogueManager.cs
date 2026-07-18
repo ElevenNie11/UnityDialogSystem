86b39de101fad79947e14d510b63a82fe5cf2dbe
Eleven <nieshiyi20060927@qq.com>
2026-07-18 15:09:58 +0800

    第一版·对话

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;   
 
 public class DialogueManager : MonoBehaviour
 {
+    [Header("UI组件")]
+    public Text textContent;
+    [Header("文本文件")]
+    public TextAsset textFile;
+    public int index;     //序号
+    //定义一个列表 list 把文本自动分成一句一句的存入列表
+    List<string> textList = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
